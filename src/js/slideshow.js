3cb13b3c6f9c81f1a843014054ce4835cc1211b2
Jonathan Freedman <jonafree@gmail.com>
2013-07-21 23:51:57 -0700

    slideshow improvements :movie_camera:

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,3 +1,17 @@
+function queryStrBit(ji) {
+    hu = window.location.search.substring(1);
+    gy = hu.split("&");
+    for (i=0;i<gy.length;i++) {
+        ft = gy[i].split("=");
+        if (ft[0] == ji) {
+            return ft[1];
+        } else {
+            return "";
+        }
+    }
+}
+
+
 function doRedirect() {
     window.location = "/game/";
 }
@@ -6,8 +20,8 @@ function loadImages() {
     $(imageList).each(function() {
                           $('<img/>').
                               attr('src', "images/" + this).
-                              attr('height', "240").
-                              attr('width', "320").
+                              //                              attr('height', "240").
+                              //                              attr('width', "320").
                               appendTo('#slides').
                               css('display', 'none');
                       });
@@ -49,7 +63,7 @@ function rollTape() {
 }
 
 function doStuff() {
-    if ( $.cookie('seen_slideshow') == "true" ) {
+    if ( queryStrBit("force") != "true" && $.cookie('seen_slideshow') == "true" ) {
         doRedirect();
     } else {
         loadImages();
